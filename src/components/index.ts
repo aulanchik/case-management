3131beb68b9b86657421f1921b9cec6e51e04661
Artyom Ulanchik <artyom.u@outlook.com>
2024-12-11 17:59:57 +0000

    enabling loader

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Checkbox } from "./Checkbox";
 export { default as Sidebar } from "./Sidebar";
 export { default as Wrapper } from "./Wrapper";
 export { default as Header } from "./Header";
+export { default as Loader } from "./Loader";
