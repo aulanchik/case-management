64a74626b7ef699fcb973851588455831d541986
Artyom Ulanchik <artyom.u@outlook.com>
2024-12-11 20:52:47 +0000

    enabling table and include in all pages

--- src/pages/AllCases.tsx
+++ src/pages/AllCases.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
-import { Wrapper, Header, Filter } from "@/components";
+import { Wrapper, Header, Filter, Table } from "@/components";
 
 const AllCases: FC = () => {
   return (
     <Wrapper>
       <Header title="All cases" />
       <Filter />
+      <Table />
     </Wrapper>
   );
 };
