943a30ad79159d023264ff387dab886f067441d2
Artyom Ulanchik <artyom.u@outlook.com>
2024-12-11 16:34:37 +0000

    wiring pages to specific paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { FC } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AllCases, AcceptedCases, PendingCases, RejectedCases } from "@/pages";
 
-const App: React.FC = () => <>Hello World</>;
+const App: FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/all" element={<AllCases />} />
+        <Route path="/accepted" element={<AcceptedCases />} />
+        <Route path="/pending" element={<PendingCases />} />
+        <Route path="/rejected" element={<RejectedCases />} />
+        <Route path="/" element={<AllCases />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
