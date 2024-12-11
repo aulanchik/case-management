aec7b1b5e815d9382cab74f30c28fb764a8c321f
Artyom Ulanchik <artyom.u@outlook.com>
2024-12-11 15:04:42 +0000

    added base utils for components

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,4 +2,15 @@ interface ImgTypes {
   [key: string]: string;
 }
 
-export type { ImgTypes };
+interface ColorTypes {
+  [key: string]: string;
+}
+interface SizeTypes {
+  [key: string]: string;
+}
+
+interface DeviceTypes {
+  [key: string]: string;
+}
+
+export type { ImgTypes, ColorTypes, SizeTypes, DeviceTypes };
---
+++ src/theme/colors.tsx
@@ -0,0 +1,12 @@
+import { ColorTypes } from "@/interfaces";
+
+const colors: ColorTypes = {
+  white: "#fff",
+  grey: "#eee",
+  red: "#af2d24",
+  blue1: "#1c4ed1",
+  blue2: "#0a65ff",
+  blue3: "#f4f7fc",
+};
+
+export default colors;
